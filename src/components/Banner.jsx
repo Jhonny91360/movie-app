5f724aaaaffe9c72dc55569df8ff180ee554508d
Jhonny91360 <jhonnyzamsa@hotmail.com>
2023-09-27 07:19:54 -0500

    Se repara ruta imagen del banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ const Banner=()=>{
 
     return(
         <div className="">
-        <img  className="mx-auto w-96" src="../../public/john-wick-3-parabellum_16.webp" alt="banner-1" />
+        <img  className="mx-auto w-96" src="john-wick-3-parabellum_16.webp" alt="banner-1" />
         </div>
     )
 }
