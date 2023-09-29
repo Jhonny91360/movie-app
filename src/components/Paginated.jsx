a4d0ae5ef8bc104b7e31086e8a28096e2d75a443
Jhonny91360 <jhonnyzamsa@hotmail.com>
2023-09-28 21:59:44 -0500

    Paginado responsive

--- src/components/Paginated.jsx
+++ src/components/Paginated.jsx
@@ -13,21 +13,32 @@ const Paginated = ({page, setPage, max}) => {
     }
 
     const createButton = (paginas)=>{  //Funcion crea botones, el boton sera mas grande si es el numero de la pagina actual
-      if(paginas===page) return (<button key={paginas}  onClick={() =>{ pageHandler(paginas) }}>{paginas}</button>)
-      else return (<button key={paginas} onClick={() => {pageHandler(paginas)}}>{paginas}</button>)
+      if(paginas===page) return (
+                                
+                                  <button key={paginas}  
+                                          className="bg-red-700 p-2 rounded-lg w-12 mx-2 text-white text-[20px] font-bold"
+                                          onClick={() =>{ pageHandler(paginas) }}> {paginas}
+                                  </button>
+                                 
+                                  )
+      else return (
+                    <button key={paginas} 
+                            className=" mx-4 text-gray-500 text-[20px] font-bold"
+                            onClick={() => {pageHandler(paginas)}}  >{paginas}
+                    </button>)
     } 
   
     
 
   return (
-    <div  >
+    <div  className="mt-10 flex flex-row justify-center">
     
-    <button  disabled={page === 1} onClick={() =>{if(page>1){pageHandler(page-1)}}}>{"<"}</button>
+    <button  disabled={page === 1} className=" mx-4 text-gray-500 text-[20px] font-bold" onClick={() =>{if(page>1){pageHandler(page-1)}}}>{"<"}</button>
     {/* Mostrar botones para cada página */}
     {pages.map((paginas) => (
       createButton(paginas) //LLamo a la funcion para crear botones
     ))}
-    <button  disabled={page === max? true:false} onClick={() => {pageHandler(page+1)}}>{">"}</button>
+    <button  disabled={page === max? true:false}  className=" mx-4 text-gray-500 text-[20px] font-bold" onClick={() => {pageHandler(page+1)}}>{">"}</button>
   </div>
   )
 }
